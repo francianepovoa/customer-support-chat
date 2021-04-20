f1506e8c0d4b44399634821fd7c72caee1dab9a8
francianepovoa <francianerpovoa@gmail.com>
2021-04-20 17:11:39 +0200

    Starting with the database

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
 import express from "express";
 
+import "./database";
+
+import { routes } from "../routes";
+
 const app = express();
 
-app.get("/", (req, res) => {
-	return res.json({
-		message: "Hello!",
-	});
-});
+app.use(express.json());
 
-app.post("/", (req, res) => {
-	return res.json({
-		message: "User saved successfully",
-	});
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running"));
